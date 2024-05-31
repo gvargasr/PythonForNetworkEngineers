97070ce50d2b828bee15b002e394cd8591036379
gvargasr <140751366+gvargasr@users.noreply.github.com>
2024-05-30 19:39:32 -0600

    loops

--- for_loops.py
+++ for_loops.py
@@ -1,13 +1,15 @@
 import netmiko
 
-ip_string = '192.168.0.7,10.254.0.2,10.254.0.3'
+ip_string = '10.254.0.1,10.254.0.2,10.254.0.3'
 
 for ip in ip_string.split(','):
     username = 'cisco'
     password = 'cisco'
     device_type = 'cisco_ios'
 
-    net_connect = netmiko.ConnectHandler(ip=ip, username=username,
+    net_connect = netmiko.ConnectHandler(ip=ip, username=username, 
                         password=password, device_type=device_type)
     sh_ip_int = net_connect.send_command('sh ip int')
     print(sh_ip_int)
+
+
---
+++ test.py
@@ -0,0 +1,7 @@
+devices = {'xrv_a': 1, 'xrv_b': 2, 'xrv_c': 3}
+x = list()
+
+for d in devices:
+    x.append(devices[d])
+
+print(x)
